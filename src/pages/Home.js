0f0c1e9f01c814670100910e240c07e34873b64b
EdwinRodriguezValle <rodriguez.valle.edwin@gmail.com>
2022-09-30 17:34:07 +0200

    this is the right one

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <section>
+            <h1>Your are watching the HOME PAGINE empty</h1>
+        </section>
+    );
+}
+
+export default Home;
